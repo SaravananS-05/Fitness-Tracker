dbe68cde81b2b7a160a28507e010cc27a9c09322
Saravanan <saravanans.22cse@kongu.edu>
2025-02-08 10:57:11 +0530

    added client

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>Hello...</h2>
+      <h1>I am Saravanan</h1>
+    </>
+  )
+}
+
+export default App
